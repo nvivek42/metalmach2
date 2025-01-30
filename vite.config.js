f405d127e0d508afe3ce3b4aba3b6e7f55140c0c
nvivek42 <viveknarole100@gmail.com>
2025-01-30 21:46:51 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/metalmach2/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
-      },
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name][extname]'
-      }
-    }
-  },
   server: {
     port: 3000,
     open: true
